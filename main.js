59a931d74dba93f774292aa1037ba4620012d1a9
nissik120 <nissik120@gmail.com>
2022-11-25 21:04:11 +0100

    EDIT: Syntax update

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let questionCount = 0;
+let questionCount = 5;
 let questionNumber = 0;
 
 const quizWrap = document.querySelector(".quiz_wrap");
@@ -6,27 +6,37 @@ const textQuestion = document.querySelector(".text_ques");
 const optQues = document.querySelector(".opt_ques");
 const btnNext = document.querySelector(".btn_next");
 
-btnNext.onclick = () =>{
- console.log("Next");
+btnNext.onclick = (questionNumber, questionCount) =>{
+    console.log("Next");
+    questionNumber<questionCount?? questionNumber++;
+}
+
+function displayOpeningTab(index){
+    const textQuestion = document.querySelector(".text_ques");
+    const optQues = document.querySelector(".opt_ques");
+
+    let textQuestionContent = `<span> Press Next To get Started. </span>`
+
+    textQuestion.innerHTML = textQuestionContent;
 }
 
 function displayCurrentQuestion(index){
     const textQuestion = document.querySelector(".text_ques");
     const optQues = document.querySelector(".opt_ques");
 
-    let textQuestionContent = `<span> Question Number 1</span>`
+    let textQuestionContent = `<span> Question Number ${index}</span>`
 
     textQuestion.innerHTML = textQuestionContent;
 
 }
 
-function questionsHandler(){
-    displayCurrentQuestion(1);
+function questionsDisplayHandler(index){
+    index==0 ? displayOpeningTab(index): displayCurrentQuestion(index);
 }
 
 
 function loadAppView(){
-    
+    questionsDisplayHandler(questionNumber);
 }
 
 document.addEventListener('DOMContentLoaded', loadAppView());
