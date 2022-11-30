0888aef053a5a9febe00bb9171fbe545542d8afe
Nissi Lawrence Kazembe <nissik120@gmail.com>
2022-11-30 11:41:24 +0100

    UPDATE: selected questions for questions.js

--- js/questions.js
+++ js/questions.js
@@ -2,71 +2,82 @@
 let questions = [
     {
     numb: 1,
-    question: "What does HTML stand for?",
+    question: "If I have to learn how to do something, I learn best when I:",
     classification: "A",
     options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
+      "Watch someone show me how",
+      "Hear someone tell me how",
+      "Try to do it myself.",
     ]
   },
     {
     numb: 2,
-    question: "What does CSS stand for?",
+    question: "When I spell, I verify accuracy by:",
     classification: "A",
     options: [
-      "Common Style Sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet"
+      "Looking at the word to see if it looks correct",
+      "Sounding the word out in my head",
+      "Getting a feeling about the correctness of the spelling.",
     ]
   },
     {
     numb: 3,
-    question: "What does PHP stand for?",
+    question: "When asked to give directions, I:",
     classification: "B",
     options: [
-      "Hypertext Preprocessor",
-      "Hypertext Programming",
-      "Hypertext Preprogramming",
-      "Hometext Preprocessor"
+      "See the actual places in my mind as I say them or prefer to draw them.",
+      "Have no difficulty giving them verbally.",
+      "Have to point or move my body as I give them",
     ]
   },
-    {
+  {
     numb: 4,
-    question: "What does SQL stand for?",
-    classification: "C",
-    options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
-    ]
-  },
-    {
-    numb: 5,
-    question: "What does XML stand for?",
+    question: "When I have to remember a list of items, I remember them best if I:"    ",
     classification: "A",
     options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
+      "Write them down",
+      "Repeat them over and over to myself",
+      "Move around and use my fingers to name each item",
     ]
   },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   classification: "Correct classification of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
+  {
+    numb: 5,
+    question: "When solving a problem, I:",
+    classification: "A",
+    options: [
+      "Write or draw diagrams to see it.",
+      "Talk myself through it.",
+      "Use and/or move objects to help me think.",
+    ]
+  },
+  {
+    numb: 6,
+    question: "When I am adding numbers, I verify my answer by:",
+    classification: "A",
+    options: [
+      "Looking at the numbers to see if they are correct.",
+      "Counting the numbers in my head or out loud.",
+      "Using my fingers to get a feeling if it is correct.",
+    ]
+  },
+  {
+    numb: 7,
+    question: "When trying to recall names, I remember:",
+    classification: "A",
+    options: [
+      "Faces more easily than names.",
+      "Names more easily than faces.",
+      "The situation or the meeting more easily than names or faces.",
+    ]
+  },
+  {
+    numb: 8,
+    question: "Before going to sleep at night, I appreciate:",
+    classification: "A",
+    options: [
+      "The room is dark.",
+      "The room is quiet.",
+      "The bed feels comfortable.",
+    ]
+  },
 ];
